2ddb12fe31ffcae5a0dac94179f08bad50dce98a
Henry Cooney <hacoo36@gmail.com>
2016-07-21 14:38:53 -0600

    added options to specify number of clients

--- fabric_startup_server/main.cc
+++ fabric_startup_server/main.cc
@@ -12,7 +12,6 @@
 #include <cassert>
 #include <csignal>
 
-#define NCLIENTS 2
 #define RECV_PORT 8080
 #define SEND_PORT 8081
 
@@ -31,7 +30,12 @@ void unexpected_shutdown(int signum) {
   exit(signum);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  int nclients = 2; 
+  if (argc > 1) {
+    nclients = strtol(argv[1], NULL, 10);
+  }
+  
   signal(SIGINT, unexpected_shutdown);
   int ret;
   char* addrs = NULL;
@@ -51,52 +55,46 @@ int main() {
   sstream << "tcp://*:" << SEND_PORT;
   zmq_bind(sender, sstream.str().c_str());
   std::cout << "Sending on: " << sstream.str().c_str() << std::endl;
-  
-  int count = 0;
-  cout << "Waiting for runtimes... " << endl;
-
-
-  // Wait for all clients to post their addresses
-  while(count < NCLIENTS) {
-    char buf[256];
-    ret = zmq_recv(receiver, buf, 256, 0);
-    assert(ret >= 0);
+  while(true) {
+    int count = 0;
+    cout << "Waiting for " << nclients << " runtimes..." << endl;
     
-    // Let first client decide the address length. If any
-    // clients disagree, something has gone wrong
-    if (count == 0) {
-      addrlen = ret;
-      addrs = (char*) malloc(addrlen*NCLIENTS);
-    } else if (ret != addrlen) {
-      std::cerr << "ERROR -- address lengths do not agree. Expected: "
-		<< addrlen << " Saw: " << ret << std::endl;
-      unexpected_shutdown(ret);
-    }
-   
-    memcpy(addrs+count*addrlen, buf, addrlen);
+
+    // Wait for all clients to post their addresses
+    while(count < nclients) {
+      char buf[256];
+      ret = zmq_recv(receiver, buf, 256, 0);
+      assert(ret >= 0);
       
-    cout << "Received request from runtime: " << count << endl;
-    cout << " Size: " << addrlen << endl;
-    cout << " Data: "
-	 << buf[0]
-	 << buf[1]
-	 << buf[2]
-	 << buf[3]
-	 << endl;
-    ++count;
-  }
+      // Let first client decide the address length. If any
+      // clients disagree, something has gone wrong
+      if (count == 0) {
+	addrlen = ret;
+	addrs = (char*) malloc(addrlen*nclients);
+      } else if (ret != addrlen) {
+	std::cerr << "ERROR -- address lengths do not agree. Expected: "
+		  << addrlen << " Saw: " << ret << std::endl;
+	unexpected_shutdown(ret);
+      }
+   
+      memcpy(addrs+count*addrlen, buf, addrlen);
+      
+      cout << "Received request from runtime: " << count << endl;
+      ++count;
+    }
 
-  // Send completed address info to each client
-  for(int i = 0; i < NCLIENTS; ++i) {
-    zmq_send(sender, addrs, addrlen*NCLIENTS, 0);
-  }
+    // Send completed address info to each client
+    for(int i = 0; i < nclients; ++i) {
+      zmq_send(sender, addrs, addrlen*nclients, 0);
+    }
 
-  if(addrs)
-    free(addrs);
+    if(addrs)
+      free(addrs);
  
-  zmq_close(sender);
-  zmq_close(receiver);
-  zmq_ctx_destroy(context);
+    zmq_close(sender);
+    zmq_close(receiver);
+    zmq_ctx_destroy(context);
   
-  return 0;
+    return 0;
+  }
 }
